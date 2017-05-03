3c467e4266d07e2165fdec4497af137e9a56757b
Antony Woods <antony@mastodonc.com>
2017-05-03 21:00:17 +0100

    WIP - added better key handling and line format

--- lctpy.py
+++ lctpy.py
@@ -1,18 +1,57 @@
 from __future__ import print_function
+from threading import Thread, Lock
 import keyboard
 import time
 import click
+import datetime
 
-from threading import Thread
+glock = Lock()
+lines = []
 
-def hookcb(ke):
-   print (ke, ke.device)
+def addline(line):
+   glock.acquire()
+   lines.append(line)
+   glock.release()
+
+def savekey(down, num):
+   ts = datetime.datetime.utcnow().strftime('%d/%m/%Y %H:%M:%S.%f')[:-3]
+   line = ('{0} - {1} - {2}'.format("P" if down else "R", num, ts))
+   print(line)
+   addline(line)
 
 class worker(Thread):
+   def __init__(self, server, port, email, device):
+      self.server = server
+      self.port = port
+      self.email = email
+      self.device = device
+      self.keys = {}
+      self.count = 0
+      super(worker, self).__init__()
+
+   def keydown(self, ke):
+      if ke.scan_code not in self.keys:
+         self.count += 1
+         self.keys[ke.scan_code] = self.count
+         savekey(True, self.count)
+
+   def keyup(self, ke):
+      if ke.scan_code in self.keys:
+         savekey(False, self.keys[ke.scan_code])
+         del self.keys[ke.scan_code]
+
+   def process(self, ke):
+      if ke.event_type == 'down':
+         self.keydown(ke)
+      else:
+         self.keyup(ke)
+
    def run(self):
-      keyboard.hook(hookcb)
+      keyboard.hook(self.process)
       while True:
          time.sleep(1)
+
+
 
 @click.command()
 @click.option('--server', default="lc.shortcut.gg", help='Address of the server')
@@ -26,7 +65,7 @@ def run(server, port, email, device):
    print("Email:\t", email)
    print("Device:\t", device)
    print("-----------------")
-   worker().start()
+   worker(server, port, email, device).start()
 
 if __name__ == '__main__':
    run()
